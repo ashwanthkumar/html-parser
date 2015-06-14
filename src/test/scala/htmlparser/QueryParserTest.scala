bee6b03e023b7379b55abafa32c5252c3c5744eb
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-06-14 13:25:41 +0530

    Adding test for attributes which have values quoted

--- src/test/scala/htmlparser/QueryParserTest.scala
+++ src/test/scala/htmlparser/QueryParserTest.scala
@@ -25,6 +25,16 @@ class QueryParserTest extends FlatSpec {
     query.matches(div) should be(false)
   }
 
+  it should "parse a query that matches an element by attributes which are quoted" in {
+    val query = QueryParser.parse("""[id="para"]""")
+    query should be(Attributes(Map("id" -> "para")))
+
+    val p = Node("p", "", Map("id" -> "para"), Nil)
+    query.matches(p) should be(true)
+    val div = Node("div", "", Map("id" -> "para2"), Nil)
+    query.matches(div) should be(false)
+  }
+
   it should "parse a query that matches elements by tagname and attributes" in {
     val query = QueryParser.parse("p[id=para]")
     query should be(TagName("p") and Attributes(Map("id" -> "para")))
